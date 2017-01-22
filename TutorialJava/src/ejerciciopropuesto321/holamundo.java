56786ad5ce98ef69fa8dbe63f570d151ae0b174f
Ivan Lynch <ivan.lynch@outlook.com.ar>
2017-01-22 13:30:02 -0300

    Primer Commit

---
+++ TutorialJava/src/ejerciciopropuesto321/holamundo.java
@@ -0,0 +1,8 @@
+package ejerciciopropuesto321;
+
+class HolaMundo {
+	public static void main(String args[]) {
+		System.out.println("Hola");
+		System.out.println("Ivan!");
+	}
+}
---
+++ TutorialJava/src/ejerciciopropuesto411/holamundo.java
@@ -0,0 +1,11 @@
+package ejerciciopropuesto411;
+
+class HolaMundo {
+	public static void main(String args[]) {
+		int primerNumero = 22;
+		int segundoNumero = 33;
+		
+		System.out.println("El producto es: ");
+		System.out.println(primerNumero * segundoNumero);
+	}
+}
